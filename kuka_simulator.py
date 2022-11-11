06692946d7a3eab70072b494c329a0d799402fff
Boyuan Liang <liangb@berkeley.edu>
2022-11-11 14:44:23 -0800

    first build of controller

--- kuka_simulator.py
+++ kuka_simulator.py
@@ -5,7 +5,7 @@ import pybullet_data
 
 class KukaSimulator():
 
-    def __init__(self, open_gui: bool = True):
+    def __init__(self, position_gain=0.03, damping_gain=0.03, force_gain=0.03):
 
         # Connect to physics server.
         pybullet.connect(pybullet.GUI)
@@ -31,8 +31,10 @@ class KukaSimulator():
         # Joint damping coefficients.
         self.jd = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
 
-        # Set control position gain.
-        self.positionGain = 0.03
+        # Control parameters.
+        self.position_gain = position_gain
+        self.damping_gain = damping_gain
+        self.force_gain = force_gain
 
         # Environmental Physics Properties.
         pybullet.setGravity(0.0, 0.0, -9.81)
@@ -40,39 +42,110 @@ class KukaSimulator():
         pybullet.enableJointForceTorqueSensor(self.kukaId, self.numJoints - 1, enableSensor=True)
 
     def getEndEffectorPosition(self) -> np.array:
-        """
-        Returns
-        -------
-        A (3,)-shaped numpy array representing the Cartesian position of the end effector in world frame.
+        """Get the position of the robot end-effector.
+
+        Returns:
+            np.array: 3D position of the robot end-effector
         """
         result = pybullet.getLinkState(self.kukaId, self.kukaEndEffectorIndex)[0]
         return np.array(result)
 
-    def getForceTorqueReading(self) -> np.array:
+    def getEndEffectorVel(self) -> np.array:
+        """Get the linear and angular velocity of robot end-effector
+
+        Returns:
+            np.array: (6,) shape, first 3 is linear velocity, last 3 is angular velocity.
         """
-        Returns
-        -------
-        A (6,)-shaped numpy array representing the reaction force-torque on the last joint,
-        organized as (Fx, Fy, Fz, Mx, My, Mz).
+        results = pybullet.getLinkState(self.kukaId, self.kukaEndEffectorIndex)[6]
+        linear, angular = results[6], results[7]
+        return np.array(linear + angular)
+
+    def getJointPosVel(self) -> np.array:
+        """Get the joint position of the 7 joints in the kuka robot arm.
+
+        Returns:
+            np.array: the joint position.
+        """
+        listed_results = pybullet.getJointStates(self.kukaId, range(self.numJoints))
+        positions = [listed_results[i][0] for i in range(self.numJoints)]
+        velocities = [listed_results[i][1] for i in range(self.numJoints)]
+        return np.array(positions), np.array(velocities)
+
+    def getForceTorqueReading(self) -> np.array:
+        """ Get the force torque response of the robot end-effector.
+
+        Returns:
+            np.array: 6D force torque response (Fx, Fy, Fz, Mx, My, Mz) on robot joint number 7.
         """
         result = pybullet.getJointState(self.kukaId, self.numJoints - 1)[2]
         return np.array(result)
 
-    def moveToPosition(self, position: np.array):
-        jointPoses = pybullet.calculateInverseKinematcis(self.kukaId, self.kukaEndEffectorIndex, position)
-        for i in range(self.numJoints):
-            pybullet.setJointMotorControl2(
-                bodyIndex=self.kukaId,
-                jointIndex=i,
-                controlMode=pybullet.POSITION_CONTROL,
-                targetPosition=jointPoses[i],
-                targetVelocity=0.0,
-                force=500,
-                positionGain=0.03,
-                velocityGain=1
-            )
+    def calculateEEJacobian(self) -> np.array:
+        """Get the Jacobian matrix, x_dot=J * p_dot, where x=(position, rotation)
 
+        Returns:
+            np.array: the 6x7 Jacobian matrix.
+        """
+        jPos, jVel = self.getJointPosVel()
+        linearJ, angularJ = pybullet.calculateJacobian(
+            bodyUniqueId=self.kukaId,
+            linkindex=self.kukaEndEffectorIndex,
+            localPosition=np.zeros(3),
+            objPositions=jPos,
+            objVelocities=jVel,
+            objAccelerations=np.zeros(6)
+        )
+        return np.array(linearJ + angularJ)
 
-k = KukaSimulator()
-while True:
-    print(k.getEndEffectorPosition())
+    def getMassMatrix(self) -> np.array:
+        """Get the mass matrix to satisfy t(q)=M(q)dotdotq
+
+        Returns:
+            np.array: shape (7, 7), the configuration mass matrix
+        """
+        massMatrix = pybullet.calculateMassMatrix(self.kukaId, self.kukaEndEffectorIndex)
+        return np.array(massMatrix)
+
+    def getOperationalMass(self) -> np.array:
+        """Get the operational space mass matrix
+
+        Returns:
+            np.array: shape (6, 6), the operational space mass matrix
+        """
+        configurationMass = self.getMassMatrix()
+        Jsts = self.calculateEEJacobian()
+        JstsDagger = np.linalg.pinv(Jsts)
+        operationalMass = JstsDagger.T @ configurationMass @ Jsts
+        return operationalMass
+
+    def wrench2JointTorque(self, targetEEForce: np.array) -> np.array:
+        """Move the robot with target contact force and velocity with impedance control.
+        The orientation is always pointing downward.
+
+        Args:
+            targetEEForce(np.array): the (3,) force vector of the end effector force in
+            global frame.
+
+        Returns:
+            np.array: the control torque to be applied to the 7 joints of the robot arm.
+        """
+        Jsts, _ = self.calculateEEJacobian()
+        tau = Jsts.T @ targetEEForce
+        return tau
+
+    def hybridControlWrench(self, targetPos: np.array, targetVel: np.array, targetForce: float) -> np.array:
+        """Compute the desired end-effector force by impedance control law.
+
+        Args:
+            targetPos (np.array): shape (2,), the target planar (x, y) coordinate to go to.
+            targetForce (float): the target z-axis force (global frame) to track.
+
+        Returns:
+            np.array: shape (3,), the desired end-effector force
+        """
+        current_pos = self.getEndEffectorPosition()[:2]
+        current_force = self.getForceTorqueReading()[2]
+        operation_inertial = self.getOperationalMass()
+        xy_force = operation_inertial @ (self.position_gain * (targetPos - current_pos))
+        z_force = self.force_gain * (targetForce - current_force)
+        return np.array(xy_force[0], xy_force[1], z_force)
