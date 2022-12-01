73829a3a06cab15e32c577a0b6740d275d2b623a
warrenhanwen <warren_hanwen@163.com>
2022-11-30 21:03:39 -0800

    turned Main.py into a function run_MPC

--- Main.py
+++ Main.py
@@ -3,12 +3,16 @@ import MPC
 import numpy as np
 import PD_controller as Pd
 
-
-if __name__ == "__main__":
-    x0 = np.array([0, 0, 0, 0, 0])
+def run_MPC(x0):
     Para.set_horizon(20)
     Para.set_time_step(20)
     Para.set_initial_state(x0)
     Para.update_system_para()
     [model, feasibility, x_opt, u_opt, j_opt] = MPC.solve_mpc()
-    # print('xOpt =', x_opt)
+    return u_opt
+
+
+if __name__ == "__main__":
+    u_opt = run_MPC(np.array([0, 0, 0, 0, 0]))
+    
+    
